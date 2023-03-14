e18bd0533b18cec30335280d449c4f3ffd66e918
shkray <cutaco.business@gmail.com>
2023-03-14 14:43:15 -0700

    made getResponce() func

--- zhopa.go
+++ zhopa.go
@@ -8,19 +8,24 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func main() {
-	url := "https://vk.com"
-
+func getResponce(url string) *http.Response {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close() //получение объекта http.Responce, обработка ошибок и закрытие чтения для избежания утечек памяти
+	defer resp.Body.Close() //получение объекта http.Responce, обработка ошибок и закрытие чтения
+	return resp
+}
+
+func main() {
+	url := "https://vk.com"
+	resp := getResponce(url)
 
 	file, err := os.Create("httpTree.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close() //создание файла для записи и обработка ошибок
+
 	httpResp, err := goquery.NewDocumentFromReader(resp.Body)
 }
