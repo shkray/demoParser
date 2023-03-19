99937056a1ac39aafec249c6e3213a6e7cccc099
shkray <cutaco.business@gmail.com>
2023-03-18 20:47:37 -0700

    unknown commit, migrate to ubuntu

--- zhopa.go
+++ zhopa.go
@@ -1,31 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func getResponce(url string) *http.Response {
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close() //получение объекта http.Responce, обработка ошибок и закрытие чтения
-	return resp
-}
-
 func main() {
-	url := "https://vk.com"
-	resp := getResponce(url)
-
-	file, err := os.Create("httpTree.txt")
+	// Выполняем запрос к странице
+	response, err := http.Get("https://www.example.com")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close() //создание файла для записи и обработка ошибок
+	defer response.Body.Close()
 
-	httpResp, err := goquery.NewDocumentFromReader(resp.Body)
+	// Создаем новый объект goquery из ответа
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Извлекаем HTML-код страницы
+	html, err := doc.Html()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Выводим полученный HTML-код на экран
+	fmt.Println(html)
 }
