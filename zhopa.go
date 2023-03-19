1953ef7b3dcff9a523071a4b05aa2c382db3735d
shkray <cutaco.business@gmail.com>
2023-03-18 21:07:48 -0700

    fix comm

--- zhopa.go
+++ zhopa.go
@@ -8,26 +8,39 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+func getDocument(url string) (*goquery.Document, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
+}
+
+func parseLaptopsPrices(url string) {
+	doc, err := getDocument(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".catalog-item").Each(func(i int, s *goquery.Selection) {
+		name := s.Find(".catalog-item__name").Text()
+		price := s.Find(".catalog-item__price-current").Text()
+		fmt.Printf("Laptop %d: %s - %s\n", i+1, name, price)
+	})
+}
+
 func main() {
-	// Выполняем запрос к странице
-	response, err := http.Get("https://www.example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-	// Создаем новый объект goquery из ответа
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Извлекаем HTML-код страницы
-	html, err := doc.Html()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Выводим полученный HTML-код на экран
-	fmt.Println(html)
+	url := "https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/"
+	parseLaptopsPrices(url)
 }
