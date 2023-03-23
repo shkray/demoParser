ead934dd2a0c30262c5afd6c4088f54b1a6f619b
shkray <cutaco.business@gmail.com>
2023-03-23 22:03:16 +1000

    pipetc

--- zhopa.go
+++ zhopa.go
@@ -1,46 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func getDocument(url string) (*goquery.Document, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return doc, nil
-}
-
-func parseLaptopsPrices(url string) {
-	doc, err := getDocument(url)
+func getDocument(url string) *goquery.Document {
+	http, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	doc.Find(".catalog-item").Each(func(i int, s *goquery.Selection) {
-		name := s.Find(".catalog-item__name").Text()
-		price := s.Find(".catalog-item__price-current").Text()
-		fmt.Printf("Laptop %d: %s - %s\n", i+1, name, price)
-	})
+	defer http.Body.Close()
+	resp, err := goquery.NewDocumentFromReader(http.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp
 }
 
 func main() {
-	url := "https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/"
-	parseLaptopsPrices(url)
+	//url := "https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/"
+	// url := "www.dns-shop.ru/ajax-state/product-buy/"
+
+	// client := &http.Client{}
+
+	// req, _ := http.NewRequest("POST", url, nil)
+
+	// req.Header.Add("X-Requested-With", "XMLHttpRequest")
+	// req.Header.Add("X-CSRF-Token", "702e3CCMcsYclqQunaxZEdxQY9TwXy6stjQstpoMGZvfHdqkVvVDrGrd0xrKmh5YryYRg4QuduTBf276w3pRog==")
+	// req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+	// req.Header.Add("Referer", "https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/no-referrer")
+
+	// resp, _ := client.Do(req)
+
+	// fmt.Print(req)
+	// fmt.Print(resp)
+
+	url := "https://www.citilink.ru/catalog/noutbuki/"
+	resp := getDocument(url)
+	file, err := os.Create("filik.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Write([]byte(resp.Text()))
 }
