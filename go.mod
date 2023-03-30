5cb50bca366ad33c3cdbe979028180c619b75a55
shkray <cutaco.business@gmail.com>
2023-03-31 09:42:18 +1000

    luti prikol

--- go.mod
+++ go.mod
@@ -2,20 +2,22 @@ module github.com/shkray/demoParser
 
 go 1.20
 
-require github.com/PuerkitoBio/goquery v1.8.1
+require github.com/chromedp/chromedp v0.9.0
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/blang/semver v3.5.1+incompatible // indirect
+	golang.org/x/net v0.7.0 // indirect
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
 	github.com/chromedp/cdproto v0.0.0-20230220211738-2b1ec77315c9 // indirect
-	github.com/chromedp/chromedp v0.9.1 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/tebeka/selenium v0.9.9 // indirect
-	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+
 )
--- zhopa.go
+++ zhopa.go
@@ -1,49 +1,70 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
-
-	"github.com/PuerkitoBio/goquery"
+	"strings"
 )
 
-func getDocument(url string) *goquery.Document {
-	http, err := http.Get(url)
+// func getDocument(url string) *goquery.Document {
+// 	http, err := http.Get(url)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer http.Body.Close()
+// 	resp, err := goquery.NewDocumentFromReader(http.Body)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return resp
+// }
+
+func clientProxy(rawProxy *string) *http.Client {
+	username := (*rawProxy)[0:strings.Index(*rawProxy, ":")]
+	password := (*rawProxy)[strings.Index(*rawProxy, ":")+1 : strings.Index(*rawProxy, "@")]
+	proxy := "https://" + strings.Replace((*rawProxy)[strings.Index(*rawProxy, "@")+1:], "\n", "", -1)
+	fmt.Print(rawProxy)
+	proxyUrl, err := url.Parse(proxy)
+	proxyAuth := url.UserPassword(username, password)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer http.Body.Close()
-	resp, err := goquery.NewDocumentFromReader(http.Body)
+	proxyUrl.User = proxyAuth
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+	return httpClient
+}
+
+func getProxyURLs() []string {
+	file, err := os.Open(".proxy")
 	if err != nil {
 		log.Fatal(err)
 	}
-	return resp
+	defer file.Close()
+	var proxyList []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		//fmt.Print(line)
+		proxyList = append(proxyList, line)
+	}
+	return proxyList
 }
 
 func main() {
-	//url := "https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/"
-	// url := "www.dns-shop.ru/ajax-state/product-buy/"
-
-	// client := &http.Client{}
-
-	// req, _ := http.NewRequest("POST", url, nil)
-
-	// req.Header.Add("X-Requested-With", "XMLHttpRequest")
-	// req.Header.Add("X-CSRF-Token", "702e3CCMcsYclqQunaxZEdxQY9TwXy6stjQstpoMGZvfHdqkVvVDrGrd0xrKmh5YryYRg4QuduTBf276w3pRog==")
-	// req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
-	// req.Header.Add("Referer", "https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/no-referrer")
-
-	// resp, _ := client.Do(req)
-
-	// fmt.Print(req)
-	// fmt.Print(resp)
-
-	url := "https://www.citilink.ru/catalog/noutbuki/"
-	resp := getDocument(url)
-	file, err := os.Create("filik.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	file.Write([]byte(resp.Text()))
+	//url := "http://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/"
+	proxyList := getProxyURLs()
+	proxyNumber := 0
+	//fmt.Print(proxyList[0], "\n")
+	client := clientProxy(&proxyList[proxyNumber])
+	fmt.Println(client)
 }
+
+//token, err := httpDoc.Find("meta[name='csrf-token']").Attr("content")   выборка csrf токена в днс
